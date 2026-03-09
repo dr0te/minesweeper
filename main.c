38670b809fb673520181bf901cf7c9fc54e1e8ba
dr0te <andrybowshik@gmail.com>
2026-03-10 01:31:54 +0200

    add field

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
+ char field[10][10];
+    int i, j;
+
 int main(){
-    printf("Minesweeper version 0.0.1");
+    printf("Minesweeper version 0.0.2");
+    // заповнення масиву
+    for(i = 0; i < 10; i++) {
+        for(j = 0; j < 10; j++) {
+            field[i][j] = '#';
+        }
+    }
+
+    // вивід масиву
+    for(i = 0; i < 10; i++) {
+        for(j = 0; j < 10; j++) {
+            printf("%c ", field[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
